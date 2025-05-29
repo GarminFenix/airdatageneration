3aece584d1972231d9c4194bd7008b6ebd4a7dbf
Ross <ross.cochrane.2024@uni.strath.ac.uk>
2025-05-29 13:38:38 +0100

    check if push causes deploy

--- src/startup.sh
+++ src/startup.sh
@@ -1,3 +1,4 @@
-export PYTHONPATH=/home/site/wwwroot/myenv/lib/python3.13/site-packages
+cd /home/site/wwwroot
+export PYTHONPATH=/home/site/wwwroot/antenv/lib/python3.13/site-packages
 /home/site/wwwroot/antenv/bin/gunicorn --bind=0.0.0.0:8000 "app:create_app()"
 
